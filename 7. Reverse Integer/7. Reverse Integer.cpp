cdbb089beab33e797679f4c028fdb4579da13446
clientinfo <85733422+clientinfo@users.noreply.github.com>
2023-10-18 15:54:02 +0200

    Update 7. Reverse Integer.cpp
    fixed formatting

--- 7. Reverse Integer/7. Reverse Integer.cpp
+++ 7. Reverse Integer/7. Reverse Integer.cpp
@@ -3,41 +3,42 @@
 class Solution {
 
 public:
-    
+
 	// Define the reverse function which takes an integer x as input.
-	int reverse(int x) 
-	{ 	
+	int reverse(int x)
+	{
+
 		// Initialize a variable to store the reversed integer.
-        int reversed_integer = 0; 
-		
-		 // Starts loop to reverse the digits of the given integer.
-        while (x != 0)
+		int reversed_integer = 0;
+
+		// Starts loop to reverse the digits of the given integer.
+		while (x != 0)
 		{
 			// Extracts the last digit of the input integer.
-            int last_digit = x % 10; 
-			
+			int last_digit = x % 10;
+
 			// Remove the last digit from the input integer.
-            x /= 10; 
+			x /= 10;
 
 			// Check for overflow condition (greater than INT_MAX) if we append the current digit.
-            if (reversed_integer > INT_MAX / 10 || (reversed_integer == INT_MAX / 10 && last_digit > 7))
+			if (reversed_integer > INT_MAX / 10 || (reversed_integer == INT_MAX / 10 && last_digit > 7))
 			{
-                
-                return 0;
-            }
-			
+
+				return 0;
+			}
+
 			// Check for overflow condition (less than INT_MIN) if we append the current digit.
-            if (reversed_integer < INT_MIN / 10 || (reversed_integer == INT_MIN / 10 && last_digit < -8))
+			if (reversed_integer < INT_MIN / 10 || (reversed_integer == INT_MIN / 10 && last_digit < -8))
 			{
-                
-                return 0; 
-            }
+
+				return 0;
+			}
 
 			// Reverse the integer by appending the current digit.
-            reversed_integer = reversed_integer * 10 + last_digit; 
-        }
-		
+			reversed_integer = reversed_integer * 10 + last_digit;
+		}
+
 		// Return the reversed integer.		
-        return reversed_integer; 
-    }
+		return reversed_integer;
+	}
 };
