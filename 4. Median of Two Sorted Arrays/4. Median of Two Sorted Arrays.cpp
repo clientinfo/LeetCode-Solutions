d8ed22b8b14e056d237c50f011e7570e523b93f2
clientinfo <85733422+clientinfo@users.noreply.github.com>
2023-10-18 16:04:09 +0200

    Update 4. Median of Two Sorted Arrays.cpp
    added some more comments

--- 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.cpp
+++ 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.cpp
@@ -2,99 +2,111 @@
 
 class Solution
 {
-
 public:
 
-	// Define the findMedianSortedArrays function which takes two sorted arrays as parameters.
-	double findMedianSortedArrays(std::vector<int>& array_number_one, std::vector<int>& array_number_two)
-	{
-		
-		// Get the sizes of the input arrays.
-		int m = array_number_one.size();
-		int n = array_number_two.size();
-		
-		// Variables to maintain the binary search range and calculate the median.
-		int imin = 0, imax = m;
-		int halfLen = (m + n + 1) / 2;
-		double median = 0.0;
+    // Define the findMedianSortedArrays function which takes two sorted arrays as parameters.
+    double findMedianSortedArrays(std::vector<int>& array_number_one, std::vector<int>& array_number_two)
+    {
+        // Get the sizes of the input arrays.
+        // Size of the first array.
+        int m = array_number_one.size(); 
 
-		// Ensure array_number_one is the smaller array to simplify the following logic.
-		if (m > n)
-		{
-			std::swap(array_number_one, array_number_two);
-			std::swap(m, n);
-		}
+        // Size of the second array.
+        int n = array_number_two.size(); 
 
-		// Perform binary search to find the median.
-		while (imin <= imax) 
-		{
-			// Current index in array_number_one.
-			int i = (imin + imax) / 2;
+        // Variables to maintain the binary search range and calculate the median.
+        // Initialize the minimum index for binary search.
+        int i_min = 0; 
 
-			// Corresponding index in array_number_two.
-			int j = halfLen - i; 
+        // Initialize the maximum index for binary search.
+        int i_max = m; 
 
-			// Binary search to find a partition that satisfies the median condition.
-			if (i < m && array_number_two[j - 1] > array_number_one[i]) 
-			{
-				imin = i + 1;
-			}
+        // Calculate the length of the first half of the merged arrays.
+        int halfLen = (m + n + 1) / 2; 
 
-			else if (i > 0 && array_number_one[i - 1] > array_number_two[j]) 
-			{
-				imax = i - 1;
-			}
+        // Initialize the median to 0.0.
+        double median = 0.0; 
 
-			else 
-			{
-				// Found a valid partition, calculate the median accordingly.
-				// Calculate the maximum of the left side of the partition.
+        // Ensure array_number_one is the smaller array to simplify the following logic.
+        if (m > n)
+        {
+            // Swap the arrays.
+            std::swap(array_number_one, array_number_two); 
+            
+            // Swap the sizes.
+            std::swap(m, n); 
+        }
 
-				int max_of_left = 0;
-				if (i == 0) 
-				{
-					max_of_left = array_number_two[j - 1];
-				}
+        // Perform binary search to find the median.
+        while (i_min <= i_max)
+        {
+            // Current index in array_number_one.
+            int i = (i_min + i_max) / 2;
 
-				else if (j == 0)
-				{
-					max_of_left = array_number_one[i - 1];
-				}
+            // Corresponding index in array_number_two.
+            int j = halfLen - i;
 
-				else 
-				{
-					max_of_left = std::max(array_number_one[i - 1], array_number_two[j - 1]);
-				}
+            // Binary search to find a partition that satisfies the median condition.
+            if (i < m && array_number_two[j - 1] > array_number_one[i])
+            {
+                i_min = i + 1;
+            }
 
-				// If the total length of the merged array is odd, return the maximum of the left side.
-				if ((m + n) % 2 == 1) 
-				{
-					return max_of_left;
-				}
+            else if (i > 0 && array_number_one[i - 1] > array_number_two[j])
+            {
+                i_max = i - 1;
+            }
 
-				// Calculate the minimum of the right side of the partition.
-				int min_of_right = 0;
+            else
+            {
+                // Found a valid partition, calculate the median accordingly.
+                // Calculate the maximum of the left side of the partition.
+                int max_of_left = 0;
 
-				if (i == m) 
-				{
-					min_of_right = array_number_two[j];
-				}
+                if (i == 0)
+                {
+                    max_of_left = array_number_two[j - 1];
+                }
 
-				else if (j == n)
-				{
-					min_of_right = array_number_one[i];
-				}
+                else if (j == 0)
+                {
+                    max_of_left = array_number_one[i - 1];
+                }
 
-				else
-				{
-					min_of_right = std::min(array_number_one[i], array_number_two[j]);
-				}
+                else
+                {
+                    max_of_left = std::max(array_number_one[i - 1], array_number_two[j - 1]);
+                }
 
-				// If the total length of the merged array is even, return the average of the left and right minima.
-				return (max_of_left + min_of_right) / 2.0;
-			}
-		}
+                // If the total length of the merged array is odd, return the maximum of the left side.
+                if ((m + n) % 2 == 1)
+                {
+                    return max_of_left;
+                }
 
-		return median;
-	}
+                // Calculate the minimum of the right side of the partition.
+                int min_of_right = 0;
+
+                if (i == m)
+                {
+                    min_of_right = array_number_two[j];
+                }
+
+                else if (j == n)
+                {
+                    min_of_right = array_number_one[i];
+                }
+
+                else
+                {
+                    min_of_right = std::min(array_number_one[i], array_number_two[j]);
+                }
+
+                // If the total length of the merged array is even, return the average of the left and right minima.
+                return (max_of_left + min_of_right) / 2.0;
+            }
+        }
+
+        return median;
+    }
 };
